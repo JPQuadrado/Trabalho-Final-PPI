b3201b9d78669b5696a740d819bc6b2646f5ea2c
r1beirin <mateusriibeiro@gmail.com>
2023-04-13 23:32:26 -0300

    created img in anuncio and moved pics to anuncio/img

--- anuncio/js/script.js
+++ anuncio/js/script.js
@@ -1,6 +1,6 @@
 const imgAnuncioDetalhado = document.getElementById("img-div-anuncio");
 const btnAnuncioDetalhado = document.getElementById("btn-div-anuncio");
-const opcoesImgAnuncio = ["../img/caneca1.jpg", "../img/caneca2.jpg", "../img/caneca3.jpg", "../img/caneca4.jpg"]
+const opcoesImgAnuncio = ["/anuncio/img/caneca1.jpg", "/anuncio/img/caneca2.jpg", "/anuncio/img/caneca3.jpg", "/anuncio/img/caneca4.jpg"]
 var index = 0;
 
 btnAnuncioDetalhado.onclick = function(){
