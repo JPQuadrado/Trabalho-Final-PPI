9eb9bff3838e548be2904e60b9a745942f12f88b
r1beirin <mateusriibeiro@gmail.com>
2023-05-15 17:10:20 -0300

    buscaCategoria feature

---
+++ httpdocs/anuncio/cadastro/buscaCategoria.php
@@ -0,0 +1,21 @@
+<?php
+
+require "../../conexaoMysql.php";
+$pdo = mysqlConnect();
+
+try {
+    $sql = <<<SQL
+        SELECT codigo, nome
+        FROM categoria
+    SQL;
+
+    $stmt = $pdo->query($sql);
+    $categorias = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    echo json_encode($categorias);
+}
+
+catch (Exception $exception){
+    exit("Falha: " . $exception->getMessage());
+}
+
+?>
--- httpdocs/anuncio/cadastro/js/script.js
+++ httpdocs/anuncio/cadastro/js/script.js
@@ -5,6 +5,8 @@ document.addEventListener("DOMContentLoaded", function(){
         enviaForm(form);
         e.preventDefault();
     }
+
+    buscarCategorias();
 });
 
 function enviaForm(form){
@@ -12,6 +14,32 @@ function enviaForm(form){
     let xhr = new XMLHttpRequest();
 
     xhr.open("POST", "../cadastroAnuncio.php");
-    console.log(xhr.responseType());
+    
     xhr.send(formData);
+}
+
+function buscarCategorias(){
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", "buscaCategoria.php");
+
+    xhr.onreadystatechange = function(){
+        if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200){
+            // Converte a resposta json em objeto JS.
+            var categorias = JSON.parse(xhr.responseText);
+            const select = document.getElementById("categoria");
+            
+            /**
+             * Faz um forEach no array recebido pelo servidor
+             * E cria uma nova opção no select dinamente.
+             */
+            categorias.forEach(function(categoria){
+                var opcao = document.createElement('option');
+                opcao.value = categoria.codigo;
+                opcao.text = categoria.nome;
+                select.add(opcao);
+            });
+        }
+    };
+
+    xhr.send();
 }
