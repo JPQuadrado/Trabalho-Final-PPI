f52a101fe12f9cd84ae2bbdd34906a75a62495fd
João Pedro Corrêa <31656134+JPQuadrado@users.noreply.github.com>
2023-05-19 18:08:46 -0300

    Jp quadrado/dev/home (#30)
    * Merge main > home (#29)
    
    * buscaCategoria feature
    
    * finished buscaEndereco with cep
    
    * documentação
    
    * fix btn
    
    * Update README.md
    
    * created cadastroAnuncio.php (no tested)
    
    * created conta/atualizar frontend
    
    * modify readme.md
    
    * modify readme.md
    
    * fix transactions
    
    * funcionalidade de modificar dados conta
    
    * Update README.md
    
    * login validation success
    
    * changed to absolute path action form to relative with form.getAttribute(action)
    
    * update readme.md
    
    * validation conta/atualizar
    
    * Update README.md
    
    * busca informações a partir do email pra atualizar conta
    
    * Update README.md
    
    * Jp quadrado/dev/home (#25)
    
    * First version of /home
    
    * Criação nav-bar em andamento - Fazer NOVA
    
    * main > quadrado (#24)
    
    * buscaCategoria feature
    
    * finished buscaEndereco with cep
    
    * documentação
    
    * fix btn
    
    * Update README.md
    
    * created cadastroAnuncio.php (no tested)
    
    * created conta/atualizar frontend
    
    * modify readme.md
    
    * modify readme.md
    
    * fix transactions
    
    * funcionalidade de modificar dados conta
    
    * Update README.md
    
    * login validation success
    
    * changed to absolute path action form to relative with form.getAttribute(action)
    
    * update readme.md
    
    * validation conta/atualizar
    
    * Update README.md
    
    * busca informações a partir do email pra atualizar conta
    
    * Update README.md
    
    ---------
    
    Co-authored-by: r1beirin <mateusriibeiro@gmail.com>
    Co-authored-by: Mateus <77507023+r1beirin@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Quadrado <quadrado@pop-os.localdomain>
    Co-authored-by: r1beirin <mateusriibeiro@gmail.com>
    Co-authored-by: Mateus <77507023+r1beirin@users.noreply.github.com>
    
    * function upload img
    
    * att readme.md
    
    * fix home/index.php
    
    * synchronize categorias in first page
    
    * rename buscar > busca
    
    * att readme.md
    
    * validation cadastro if input empty
    
    * fix indentação
    
    * change id to class
    
    * added envio interesse
    
    ---------
    
    Co-authored-by: r1beirin <mateusriibeiro@gmail.com>
    Co-authored-by: Mateus <77507023+r1beirin@users.noreply.github.com>
    Co-authored-by: Quadrado <quadrado@pop-os.localdomain>
    
    * Front end HOME - Finalizado
    
    ---------
    
    Co-authored-by: r1beirin <mateusriibeiro@gmail.com>
    Co-authored-by: Mateus <77507023+r1beirin@users.noreply.github.com>
    Co-authored-by: Quadrado <quadrado@pop-os.localdomain>

---
+++ httpdocs/anuncio/interesses/index.php
@@ -0,0 +1,52 @@
+<?php
+require "../conexaoMysql.php";
+$pdo = mysqlConnect();
+
+try{
+    $sql = <<<SQL
+    SELECT *
+    FROM interesse
+
+    
+    SQL;
+    // 55 seria o valor 'anunciante.codigo', aprendera em seção.
+
+    // Neste exemplo não é necessário utilizar prepared statements
+    // porque não há possibilidade de injeção de SQL, 
+    // pois nenhum parâmetro é utilizado na query SQL
+    $stmt = $pdo->query($sql);
+}
+catch (Exception $e) {
+    exit('Ocorreu uma falha: ' . $e->getMessage());
+}
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Home - Anunciante</title>
+</head>
+
+<body>
+    <nav class="navbar">
+      <div class="container-fluid">
+        <a class="navbar-brand center" href="#"><span id="emoji_solo_logo">🤝 Feira.</span></a>
+        <div class="d-flex gap-2 mb-3">
+          <a href="/anuncio/cadastro/" class="btn btn-success">Novo Anuncio</a>
+          <a href="/anuncio/interesse/index.php" class="btn btn-secondary">Interesses</a>
+          <a href="/" class="btn btn-outline-danger">Logout</a>
+        </div>
+      </div>      
+    </nav>
+
+    <main class="container container_n">
+    </main>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+</body>
+</html>
