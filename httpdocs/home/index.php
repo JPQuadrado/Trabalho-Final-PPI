f7a0b87e278ec8c0ff662a34bc3be5459897d0b6
João Pedro <j.pedrocorrea13@gmail.com>
2023-05-19 18:07:25 -0300

    Front end HOME - Finalizado

--- httpdocs/home/index.php
+++ httpdocs/home/index.php
@@ -10,7 +10,7 @@ try{
 
     
     SQL;
-    // 1 seria o valor 'anunciante.codigo', aprendera em seção.
+    // 55 seria o valor 'anunciante.codigo', aprendera em seção.
 
     // Neste exemplo não é necessário utilizar prepared statements
     // porque não há possibilidade de injeção de SQL, 
@@ -34,7 +34,17 @@ catch (Exception $e) {
 </head>
 
 <body>
-    <nav class="navbar">Feira.</nav>
+    <nav class="navbar">
+      <div class="container-fluid">
+        <a class="navbar-brand center" href="#"><span id="emoji_solo_logo">🤝 Feira.</span></a>
+        <div class="d-flex gap-2 mb-3">
+          <a href="/anuncio/cadastro/" class="btn btn-success">Novo Anuncio</a>
+          <a href="/anuncio/interesse/index.php" class="btn btn-secondary">Interesses</a>
+          <a href="/" class="btn btn-outline-danger">Logout</a>
+        </div>
+      </div>      
+    </nav>
+
     <main class="container container_n">
         <table class="table table-striped">
             <thead>
@@ -78,20 +88,30 @@ catch (Exception $e) {
 
                     echo <<<HTML
                         <tr>
-
                             <th scope="row">$codigo</th>
                             <td>$titulo</td>
                             <td>$descricao</td> 
                             <td>$preco</td>
-
-                        </tr>  
+                            <td class="btn-container">
+                                <button class="btn btn-danger" action="">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+                                  <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
+                                </svg>
+                              </button>       
+                              <button class="btn btn-primary">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye-fill" viewBox="0 0 16 16">
+                                  <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z"/>
+                                  <path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z"/>
+                                </svg>
+                              </button>
+                            </td>
+                        </tr>
                     HTML;
                 }
             ?>
             </tbody>
           </table>
     </main>
-    <footer></footer>
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </body>
