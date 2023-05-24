83a74535c024441988eaf9545b21e9ac0c28539e
r1beirin <mateusriibeiro@gmail.com>
2023-05-24 16:51:57 -0300

    fix: fix manual insertion in tabelas.sql

--- tabelas.sql
+++ tabelas.sql
@@ -56,25 +56,3 @@ INSERT INTO categoria VALUES (default, "Eletroeletrônico", "A categoria de elet
 INSERT INTO categoria VALUES (default, "Imóvel", "A categoria de imóvel engloba uma ampla variedade de propriedades, incluindo residenciais, comerciais, industriais e terrenos.");
 INSERT INTO categoria VALUES (default, "Vestuário", "A categoria de vestuário engloba todas as peças de roupa, calçados e acessórios que são usados para cobrir e proteger o corpo.");
 INSERT INTO categoria VALUES (default, "Outros", "A categoria 'outros' é uma classificação ampla que engloba uma ampla variedade de itens que não se enquadram em outras categorias mais específicas.");
-
-INSERT INTO anunciante (nome, cpf, email, hash_senha, telefone)
-VALUES ('Anunciante 1', '123.456.789-01', 'anunciante1@email.com', 'hash123', '1234567890'),
-       ('Anunciante 2', '987.654.321-01', 'anunciante2@email.com', 'hash456', '0987654321'),
-       ('Anunciante 3', '111.222.333-01', 'anunciante3@email.com', 'hash789', '1112223330');
-       
-INSERT INTO anuncio (cod_categoria, cod_anunciante, titulo, descricao, preco, data_hora, cep, bairro, cidade, estado)
-VALUES (1, 1, 'Anúncio 1', 'Descrição do Anúncio 1', 100.00, NOW(), '12345-678', 'Bairro 1', 'Cidade 1', 'Estado 1'),
-       (2, 1, 'Anúncio 2', 'Descrição do Anúncio 2', 200.00, NOW(), '23456-789', 'Bairro 2', 'Cidade 2', 'Estado 2'),
-       (3, 1, 'Anúncio 3', 'Descrição do Anúncio 3', 300.00, NOW(), '34567-890', 'Bairro 3', 'Cidade 3', 'Estado 3');
-       
-INSERT INTO interesse (mensagem, data_hora, contato, cod_anuncio)
-VALUES ('Mensagem anúncio 1', NOW(), 'Contato anúncio 1', 1),
-       ('Mensagem anúncio 2', NOW(), 'Contato anúncio 2', 1),
-       ('Mensagem anúncio 3', NOW(), 'Contato anúncio 3', 2),
-       ('Mensagem anúncio 4', NOW(), 'Contato anúncio 4', 2),
-       ('Mensagem anúncio 5', NOW(), 'Contato anúncio 5', 3),
-       ('Mensagem anúncio 6', NOW(), 'Contato anúncio 6', 3);
-       
-INSERT INTO interesse (mensagem, data_hora, contato, cod_anuncio)
-VALUES ('Mensagem anúncio 5 - Quero pagar 50reais em', NOW() - INTERVAL 1 DAY, '8558231', 3),
-       ('Mensagem anúncio 6 - Dou 3 paieros na peça', NOW() - INTERVAL 2 DAY, 'Contato anúncio 6', 3);
