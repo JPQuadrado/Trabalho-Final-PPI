77e01372808d96ea6109ddb710f70f198e14052b
Mateus <77507023+r1beirin@users.noreply.github.com>
2023-05-27 18:03:42 -0300

    feat: delete images from anuncio when delete anuncio (#72)

--- home/exclui-anuncio.php
+++ home/exclui-anuncio.php
@@ -5,10 +5,24 @@ $pdo = mysqlConnect();
 $codigo = $_GET["codigo"] ?? "";
 
 try {
+    $sqlFotos = <<<SQL
+        SELECT nome_arquivo_foto
+        FROM foto
+        WHERE cod_anuncio = ?
+        SQL;
+    
+    $stmtFotos = $pdo->prepare($sqlFotos);
+    $stmtFotos->execute([$codigo]);
+    $fotos = $stmtFotos->fetchAll(PDO::FETCH_COLUMN);
+
+    foreach($fotos as $foto){
+        unlink("../anuncio/img/" . $foto);
+    }
+
     $sql = <<<SQL
         DELETE FROM anuncio
         WHERE codigo = ?
-        LIMIT 1      
+        LIMIT 1
     SQL;
     
     $stmt = $pdo->prepare($sql);
