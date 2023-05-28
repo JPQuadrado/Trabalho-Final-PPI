2d94a2fb1cfa99e1f5f4e95e817c564e3fd47abc
Mateus <77507023+r1beirin@users.noreply.github.com>
2023-05-28 13:09:42 -0300

    Ribeirin/dev/anuncio detalhado (#73)
    * feat: anuncio getting infos, but not image
    
    * feat: anuncio detalhado working 100%

--- anuncio/js/script.js
+++ anuncio/js/script.js
@@ -1,23 +1,31 @@
-const imgAnuncioDetalhado = document.querySelector(".img-div-anuncio");
-const btnAnuncioDetalhado = document.querySelector(".btn-div-anuncio");
-const opcoesImgAnuncio = ["/anuncio/img/caneca1.jpg", "/anuncio/img/caneca2.jpg", "/anuncio/img/caneca3.jpg", "/anuncio/img/caneca4.jpg"]
 var index = 0;
-
-btnAnuncioDetalhado.onclick = function(){
-    index += 1;
-    index = index % opcoesImgAnuncio.length;
-    
-    imgAnuncioDetalhado.setAttribute('src', opcoesImgAnuncio[index])
-}
 
 document.addEventListener("DOMContentLoaded", function(){
     const form = document.querySelector(".interesse-form");
+    const btnNext = document.querySelector(".btn-div-anuncio");
+    
+    btnNext.onclick = function(){
+        nextImage();
+    }
 
     form.onsubmit = function (e) {
         enviaForm(form);
         e.preventDefault();
     }
 });
+
+function nextImage(){
+    const imgAnuncioDetalhado = document.querySelector(".img-div-anuncio");
+    const fotosDataDiv = document.getElementById('fotos-data');
+    // Pego os valores das fotos do php pelo atributo data.
+    const fotosData = fotosDataDiv.dataset.fotos;
+    const fotosArray = JSON.parse(fotosData);
+    
+    index += 1;
+    index = index % fotosArray.length;
+    
+    imgAnuncioDetalhado.setAttribute('src', '/anuncio/img/' + fotosArray[index])
+}
 
 function enviaForm(form){
     let xhr = new XMLHttpRequest();
